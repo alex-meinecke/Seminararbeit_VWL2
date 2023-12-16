d0718af5418ba713657f21aa602923ef3e66dd36
alex-meinecke <alexandermeinecke@icloud.com>
2023-12-16 15:32:42 +0100

    new compile files for biber

--- compile_praxisbericht.sh
+++ compile_praxisbericht.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 pdflatex -synctex=1 -interaction=nonstopmode Praxisbericht.tex
+biber Praxisbericht
+pdflatex -synctex=1 -interaction=nonstopmode Praxisbericht.tex
+pdflatex -synctex=1 -interaction=nonstopmode Praxisbericht.tex
